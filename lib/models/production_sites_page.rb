80df69d9656070e80b035a86c819822716f920dc
Will Cannings <me@willcannings.com>
2011-11-16 21:17:39 +1100

    Updated sites page to work with latest record proxy page interface

--- lib/models/production_sites_page.rb
+++ lib/models/production_sites_page.rb
@@ -1,12 +1,16 @@
 class ProductionSitesPage < RecordProxyPage
   # record proxy pages deal with site models (site.model_name). Override the methods it uses
   # to interact with these models we can edit Sites (a non site model)
-  def record
-    @record ||= Site.find(BSON::ObjectId.from_string(params['id']))
+  def find_record(id)
+    Site.find(id)
   end
   
-  def records
-    @records ||= Site.all
+  def all_records
+    Site.all
+  end
+  
+  def construct_record
+    Site.new
   end
   
   # get list of sites
